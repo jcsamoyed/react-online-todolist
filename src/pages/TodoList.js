c255d3e6189178d41c8987476503cd082ba7cc7f
jcsamoyed <aruby5223@gmail.com>
2022-09-02 18:32:10 +0800

    feat(todolist): 完成新增、刪除、修改代辦清單功能

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,14 +1,64 @@
+import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
+import { apiGetTodos, apiPostTodos, apiDeleteTodos, apiToggleTodos } from "@/utils/api";
 
 function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [content, setContent] = useState('');
+
+  const renderTodos = () => {
+    apiGetTodos().then((res) => {
+      const response = res.data.todos;
+      setTodos(response);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    renderTodos();
+  }, [])
+
+  const handleSetTodos = () => {
+    const todo = { content }
+    if (content) {
+      apiPostTodos(todo).then((res) => {
+        renderTodos();
+        setContent('');
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+
+  const handleToggleItem = (id) => {
+    apiToggleTodos(id)
+      .then((res) => {
+        renderTodos();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  const handleDeleteItem = (id) => {
+    apiDeleteTodos(id)
+      .then((res) => {
+        renderTodos();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   return (
     <div className="bg-half min-h-screen px-8">
       <Navbar />
       <div className="mt-4 sm:mt-10 w-full max-w-[500px] mx-auto">
         {/* 輸入框 */}
         <div className="relative mb-4 shadow-lg">
-          <input className="w-full rounded py-3 px-4" placeholder="新增待辦事項" />
-          <div className="absolute right-1 top-1 bg-black p-[10px] rounded cursor-pointer">
+          <input value={content} onChange={e => setContent(e.target.value)} className="w-full rounded py-3 px-4" placeholder="新增待辦事項" />
+          <div onClick={handleSetTodos} className="absolute right-1 top-1 bg-black p-[10px] rounded cursor-pointer">
             <img src={require('@/assets/images/plus.png')} alt="+" />
           </div>
         </div>
@@ -23,13 +73,19 @@ function TodoList() {
           {/* 列表 */}
           <div className="py-2 px-6">
             <ul>
-              <li className="group relative border-b border-gray-100">
-                <label className="flex items-center py-4 cursor-pointer">
-                  <input className="mr-4 w-5 h-5" type="checkbox" />
-                  <span>倒垃圾</span>
-                </label>
-                <img src={require('@/assets/images/delete.png')} alt="x" className="hidden absolute group-hover:block right-0 top-5 cursor-pointer" />
-              </li>
+              {
+                todos.map((item) => {
+                  return (
+                    <li key={item.id} className="group relative border-b border-gray-100">
+                      <label className="flex items-center py-4 cursor-pointer">
+                        <input className="mr-4 w-5 h-5" type="checkbox" defaultChecked={item.completed_at} onChange={() => handleToggleItem(item.id)} />
+                        <span className={item.completed_at ? 'text-gray-400 line-through' : ''}>{item.content}</span>
+                      </label>
+                      <img src={require('@/assets/images/delete.png')} alt="x" onClick={() => handleDeleteItem(item.id)} className="hidden absolute group-hover:block right-0 top-5 cursor-pointer" />
+                    </li>
+                  )
+                })
+              }
             </ul>
             {/* 表尾 */}
             <div className="flex justify-between py-4">
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,12 @@
 import { request } from '@/utils/request';
 
+// Users
 export const apiUserSignUp = data => request.post('/users', data);
 export const apiUserSignIn = data => request.post('/users/sign_in', data);
 export const apiUserSignOut = data => request.delete('/users/sign_out', data);
+
+// Todos
+export const apiGetTodos = data => request.get('/todos', data);
+export const apiPostTodos = data => request.post('/todos', data);
+export const apiDeleteTodos = id => request.delete(`/todos/${id}`);
+export const apiToggleTodos = id => request.patch(`/todos/${id}/toggle`);
